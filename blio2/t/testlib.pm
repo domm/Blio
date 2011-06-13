2ba4f3fd3691651f891f8c849e5d1ee2a6812e0e
Thomas Klausner <domm@plix.at>
2011-06-13 12:06:15 +0200

    testlib to setup blio etc

---
+++ blio2/t/testlib.pm
@@ -0,0 +1,24 @@
+package testlib;
+use 5.010;
+use strict;
+use warnings;
+
+use File::Temp qw(tempdir);
+use Test::File;
+
+use Blio;
+
+sub blio {
+    my $site = shift;
+    return Blio->new(
+        output_dir=>testdir(),
+        source_dir=>Path::Class::dir(qw(. t testdata ),$site),
+        templates_dir=>Path::Class::dir(qw(. share templates)),
+    );
+}
+
+sub testdir {
+    return Path::Class::dir( tempdir( CLEANUP => $ENV{NO_CLEANUP} ? 0 : 1 ));
+}
+
+1;
